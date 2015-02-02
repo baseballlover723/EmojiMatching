94666fd2ff66254ff9861005b8eda0aea6f05ea5
Philip Ross <rosspa@rose-hulman.edu>
2015-02-01 23:37:10 -0500

    initial commit

--- EmojiMatching/ViewController.swift
+++ EmojiMatching/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ipadNavBar: UINavigationBar!
+    
+    @IBOutlet var buttons: [UIButton]!
+    
+    
+    
+    var game = EmojiMatchingGame(numbOfPairs: 10)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateview()
+        // sets font size to 64 for iphones and 100 for ipads
+        var buttonSize = UIDevice.currentDevice().userInterfaceIdiom == .Phone ? 64 : 100
+        for button in buttons {
+            var font = button.titleLabel?.font.fontWithSize(CGFloat(buttonSize))
+            button.titleLabel?.font = font
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +35,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func pressedButton(sender: AnyObject) {
+        var button = sender as UIButton
+        var needAgain = game.pickCard(button.tag)
+        println("pressed button #\(button.tag): \(game.cards[button.tag])")
+        updateview()
+        // I know this is ugly, but I kinda ran short on time
+        if needAgain {
+            delay(1.2) {
+                self.updateview()
+            }
+        }
+        
+    }
+    
+    
+    @IBAction func pressedNewGame(sender: AnyObject) {
+        println("pressed new game")
+        game = EmojiMatchingGame(numbOfPairs: 10)
+        updateview()
+    }
+    
+    func updateview() {
+        var index = 0
+        for button in buttons {
+            var emoji = game.cards[index]
+            var text = emoji.solved ? "" : emoji.shown ? String(game.cards[index].name) : String(game.getCardBack())
+            button.setTitle(text, forState: UIControlState.Normal)
+            index++
+        }
+    }
 
+    func delay(delay:Double, closure:()->()) {
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                Int64(delay * Double(NSEC_PER_SEC))
+            ),
+            dispatch_get_main_queue(), closure)
+    }
 }
 
